4812b37ec45538a17678de15a609372db4948a90
Henrik Mader <henrik.mader@gmx.at>
2021-02-22 14:08:06 +0100

    Updated function to incorporate Xavier's

--- myfirstpackage/lib.py
+++ myfirstpackage/lib.py
@@ -1,2 +1,10 @@
+from xavtest import lib
+
 def try_me():
-    return "Hello there"
+    print("Do you want to try me?")
+    Answer = input()
+    if lib.try_me(Answer.lower()) == True:
+        return "Hello there."
+    if lib.try_me(Answer.lower()) == False:
+        return "I find your lack of faith disturbing."
+
